ccb19059292d75b99f65891205243bd8d1a0d6ee
TOR968 <tor968@yahoo.com>
2021-01-31 16:03:29 +0200

    Update ContactListItemStyles.js

--- src/components/ContactList/ContactListItemStyles.js
+++ src/components/ContactList/ContactListItemStyles.js
@@ -8,7 +8,6 @@ const ListItem = styled.li`
 `;
 
 const Text = styled.p`
-  color: #fa8072;
   margin: 0;
 `;
 
@@ -17,23 +16,11 @@ const Button = styled.button`
   outline: none;
   padding: 2px 5px;
   border-radius: 10px;
-  background-image: linear-gradient(
-    45deg,
-    #6ab1d7 0%,
-    #33d9de 50%,
-    #002878 100%
-  );
+  background-color: #fa8072;
   background-position: 100% 0;
   background-size: 200% 200%;
   font-size: 16px;
   color: white;
-  box-shadow: 0 16px 32px 0 rgba(0, 40, 120, 0.35);
-  transition: 0.5s;
-
-  &:hover {
-    box-shadow: 0 0 0 0 rgba(0, 40, 120, 0);
-    background-position: 0 0;
-  }
 `;
 
 export { ListItem, Button, Text };
